228526d97aa6a0f6b00bc8db865055066f212e7f
K RND <yonbanki@gmail.com>
2018-03-01 11:02:37 +0900

    create router

---
+++ app/javascript/containers/form/quote_create_form_container.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+export default class QuoteCreateForm extends React.Component {
+  render(){
+    return (
+      <h1>TODO: This space is used for Quote Create From</h1>
+    );
+  }
+}
+
--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -4,6 +4,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import PropTypes from 'prop-types'
 import Quote from '../components/quote'
 
@@ -11,16 +12,18 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import Quotes from '../containers/quotes_container'
+import QuoteCreateForm from '../containers/form/quote_create_form_container'
 import reducer from '../reducers/quotes_reducer'
 
-//TODO const history = createHistory()
-//TODO const middleware = routerMiddleware(history)
+//const history = createHistory();
+//const middleware = routerMiddleware(history);
 
-const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 const Hello = props => (
   <div>
   </div>
-)
+);
 
 Hello.defaultProps = {
   name: 'David'
@@ -30,10 +33,22 @@ Hello.propTypes = {
   name: PropTypes.string
 }
 
+
+var Routes = (
+  <Router>
+    <div>
+      <Link to="/">Quote</Link>
+      <Link to="/quote-create-form">Form</Link>
+      <Route exact path="/" render={({match})=>{return(<Quotes tag={1} match={match} />);}} />
+      <Route path = "/quote-create-form" component={QuoteCreateForm} />
+    </div>
+  </Router>
+);
+
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Provider store={store}>
-      <Quotes tag={1} />
+      {Routes}
     </Provider>,
     document.body.appendChild(document.createElement('div')),
   )
