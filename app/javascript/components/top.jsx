1958b8eafd69d4d42ee1134d192183919209d541
K RND <yonbanki@gmail.com>
2018-03-08 01:02:23 +0900

    css-load environment

--- app/javascript/components/top.jsx
+++ app/javascript/components/top.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
+import Style from '../../scss/top.scss'
 export default class Quote extends React.Component {
   render() {
     return (
-      <div className="top">
+      <div className={Style.top}>
         Hello_World
       </div>
     );
