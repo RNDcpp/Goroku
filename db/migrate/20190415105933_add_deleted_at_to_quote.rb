06c507602cf6f0931b0202a4f895e32ee5e472fb
RNDcpp <yonbanki@gmail.com>
2019-04-16 01:12:50 +0900

    deleted_atを追加する処理を追加

--- db/migrate/20190415105933_add_deleted_at_to_quote.rb
+++ db/migrate/20190415105933_add_deleted_at_to_quote.rb
@@ -1,4 +1,5 @@
 class AddDeletedAtToQuote < ActiveRecord::Migration[5.1]
   def change
+    add_column :quotes, :deleted_at, :datetime,:null => true, :default => nil
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180225015651) do
+ActiveRecord::Schema.define(version: 20190415105933) do
 
   create_table "quote_tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "tag_id"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180225015651) do
     t.string "text", limit: 400, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
   end
 
   create_table "tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
